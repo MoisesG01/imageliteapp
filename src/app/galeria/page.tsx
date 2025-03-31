a97b84123c95634fb74c73b5f306dca89a33ca44
Moises <moises.goncalves@olxbr.com>
2025-03-30 22:25:09 -0300

    create image card component

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,9 +1,46 @@
-import { Template } from "@/components/Template";
+import { Template, ImageCard } from "@/components";
 
 export default function GaleriaPage() {
   return (
     <Template>
-      <h1>Galeria</h1>
+      <section className="grid grid-cols-3 gap-8">
+        <ImageCard
+          src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/z/u/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752"
+          nome="Natureza"
+          tamanho="10MB"
+          dataUpload="30/03/204"
+        />
+        <ImageCard
+          src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/z/u/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752"
+          nome="Natureza"
+          tamanho="10MB"
+          dataUpload="30/03/204"
+        />
+        <ImageCard
+          src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/z/u/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752"
+          nome="Natureza"
+          tamanho="10MB"
+          dataUpload="30/03/204"
+        />
+        <ImageCard
+          src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/z/u/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752"
+          nome="Natureza"
+          tamanho="10MB"
+          dataUpload="30/03/204"
+        />
+        <ImageCard
+          src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/z/u/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752"
+          nome="Natureza"
+          tamanho="10MB"
+          dataUpload="30/03/204"
+        />
+        <ImageCard
+          src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/z/u/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752"
+          nome="Natureza"
+          tamanho="10MB"
+          dataUpload="30/03/204"
+        />
+      </section>
     </Template>
   );
 }
